e755c39cd52cc69b4ed7da628b3e5b285100562a
AlicesVoid <hypnocarrot@csu.fullerton.edu>
2023-09-11 21:07:01 -0700

    created myinv in bslashtx.m

--- MATH-340/Toolbox_MATLAB/bslashtx.m
+++ MATH-340/Toolbox_MATLAB/bslashtx.m
@@ -1,4 +1,4 @@
-function x = bslashtx(A,b)
+function [x,B] = bslashtx(A,b)
 % BSLASHTX  Solve linear system (backslash)
 % x = bslashtx(A,b) solves A*x = b
 
@@ -33,6 +33,8 @@ y = forward(L,b(p));
 % Back substitution
 x = backsubs(U,y);
 
+B = myinv(A);
+
 
 % ------------------------------
 
@@ -58,3 +60,26 @@ for k = n-1:-1:1
    j = k+1:n;
    x(k) = (x(k) - U(k,j)*x(j))/U(k,k);
 end
+
+%-----------------------------
+function A_inv = myinv(A)
+% MYINV Compute the inverse of a matrix A using bslashtx.
+
+% Perform LU decomposition with pivoting
+[L, U, p] = lutx(A);
+
+% Identity matrix of the same size as A
+[n, ~] = size(A);
+I = eye(n);
+
+% Initialize the inverse matrix
+A_inv = zeros(n);
+
+% Solve for each column of the identity matrix
+for k = 1:n
+    e_k = I(:, k);  % Extract the k-th column of the identity matrix
+    x_k = bslashtx(L, e_k(p));  % Solve for x_k using bslashtx
+    y_k = backsubs(U, x_k);  % Solve for y_k using back substitution
+    A_inv(:, k) = y_k;  % Set the k-th column of the inverse matrix
+end
+
