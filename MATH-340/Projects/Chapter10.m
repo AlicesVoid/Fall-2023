4ff10aa062feb44f5886f7494042817546a86dc6
Alice Void <hypnocarrot@csu.fullerton.edu>
2023-09-13 12:40:02 -0700

    modified ch10 code

--- MATH-340/Projects/Chapter10.m
+++ MATH-340/Projects/Chapter10.m
@@ -3,7 +3,9 @@
 % Problem 8:
 F = gallery('frank',12);
 P = pascal(12);
-F_e = eig(F);
+F_e = sort(eig(F));
 F_cn = condeig(F);
-P_e = eig(P);
+P_e = sort(eig(P));
 P_cn = condeig(P);
+(F_e(1:6).*F_e(12:-1:7))-ones(6,1);
+(P_e(1:6).*P_e(12:-1:7))-ones(6,1);
