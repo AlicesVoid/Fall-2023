48c5d8df44eb74351a1818230c0cb1694b11bfc4
AlicesVoid <hypnocarrot@csu.fullerton.edu>
2023-09-18 21:58:18 -0700

    problem 4 coded

--- MATH-340/Projects/Chapter10.m
+++ MATH-340/Projects/Chapter10.m
@@ -1,6 +1,7 @@
 % Chapter 10 Homework 
 
 % Problem 8:
+%{
 F = gallery('frank',12);
 P = pascal(12);
 F_e = sort(eig(F));
@@ -9,3 +10,28 @@ P_e = sort(eig(P));
 P_cn = condeig(P);
 (F_e(1:6).*F_e(12:-1:7))-ones(6,1);
 (P_e(1:6).*P_e(12:-1:7))-ones(6,1);
+%}
+
+% Problem 4:
+
+% Define the matrix size
+n = 101;
+
+% Create the tridiagonal matrix A
+d = ones(n-1, 1);
+A = diag(d, 1) + diag(d, -1);
+
+% Compute the eigenvalues of A
+e = eig(A);
+
+% Create a plot
+x = -(n-1)/2 : (n-1)/2;
+plot(x, e, '.');
+
+% Label the axes
+xlabel('Eigenvalue Index');
+ylabel('Eigenvalue');
+
+% Title and grid
+title('Eigenvalues of Tridiagonal Matrix A');
+grid on;
