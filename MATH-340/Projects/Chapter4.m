1413808e7bc16d4ad64c2f8747e5c113ae28d6b8
AlicesVoid <hypnocarrot@csu.fullerton.edu>
2023-10-01 18:17:20 -0700

    m340 - Progress on Chapter 4

--- MATH-340/Projects/Chapter4.m
+++ MATH-340/Projects/Chapter4.m
@@ -25,7 +25,7 @@ disp(['Complex root found: x = ', num2str(root)]);
 %}
 
 % Question 3: 
-%
+%{
 % Init. Polynomial 
 syms x;
 p_s = 816*x^3 - 3835*x^2 + 6000*x - 3125;
@@ -36,4 +36,33 @@ R = roots(sym2poly(p_s));
 
 % FzeroTX along [1,2]
 F = fzerotx(p, [1,2]);
-%
+%}
+
+% Question 9:
+%{
+%  Original Inequality 
+f = @(x) x - tan(x);
+
+% Solutions and Guesses 
+sol = 0:1:9;
+guess = ones(1,10) + 0:pi:10*pi;
+
+% Iterate through the guesses
+for i = 1:10
+    % Check if sol(i) is already a solution to f
+    if abs(f(sol(i))) < 1e-6
+        % sol(i) is already a solution, no change needed
+        continue;
+    end
+    % approx a negative guess
+    q = 0.18*pi;
+    % Use fzerotx to find a solution within [guess(i), guess(i+1)]
+    sol(i) = fzerotx(f, [guess(i), guess(i)+q]);
+end
+
+% Display the results
+disp(sol);
+%}
+
+% Question 10:
+
