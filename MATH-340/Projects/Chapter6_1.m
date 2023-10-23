f6d4b5338360d03b0bda60def6db6d4e1e008afa
Alice Void <hypnocarrot@csu.fullerton.edu>
2023-10-23 12:17:50 -0700

    m340: presented Ch6 Pr4

--- MATH-340/Projects/Chapter6_1.m
+++ MATH-340/Projects/Chapter6_1.m
@@ -1,22 +1,17 @@
 % Homework 6 Part 1: 
 
-% Question 1: 
-
-% Question 2:
-
-% Question 3:
-
 % Question 4: 
 %
 f = @(x) 2 ./ (1+x.^2);
 qtx_vals = zeros(1,16);
 qtx_steps = zeros(1,16);
 qtx_diff = zeros(1,16);
+%qtx_diff = [];
 fprintf('\n\n');
 fprintf('Actual Pi Value: %.17f\n', pi);
 fprintf('------------------------------\n');
 %figure(1); hold on; 
-for i=1:1:20
+for i=1:1:15
     tol = 10^(-i);
     [qtx_vals(i), qtx_steps(i)] = quadtx(f, -1, 1, tol);
     qtx_diff(i) = (pi - qtx_vals(i));
@@ -28,5 +23,6 @@ for i=1:1:20
     %plot(i, qtx_vals(i), 'o');
     %plot(i, qtx_diff(i), 'o');
 end
-
-% Question 6: 
+%figure(1); hold on;
+%plot(10.^(-1:-1:-15), qtx_diff, 'o');
+%}
