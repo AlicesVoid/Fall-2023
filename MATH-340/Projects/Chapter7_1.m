61cfd8fe0e7da6108580e1d0cf4dfa7ccb75ba43
Alice Void <hypnocarrot@csu.fullerton.edu>
2023-11-13 11:44:37 -0800

    m340: ch7 q6 complete

--- MATH-340/Projects/Chapter7_1.m
+++ MATH-340/Projects/Chapter7_1.m
@@ -4,12 +4,14 @@
 %
 
 % Initialize the Problem
+syms y(t) t;
+odesym = diff(y,t) == -1000*(y-sin(t))+cos(t); 
 ode = @(t, y) -1000*(y - sin(t)) + cos(t);
 y0 = 1; 
 tspan = [0 1];
 
-% Solution By Hand: 
-[t_e, y_e] = myEuler(ode, tspan, y0, 1e-3);
+% Solution By dsolve: 
+y_ds(t) = dsolve(odesym, y(0)==1);
 
 % Solve using ode23s and ode23tx
 [t_23, y_23] = ode23s(ode, tspan, y0);
@@ -21,13 +23,17 @@ label_tx = append('ode23tx: ',num2str(steps_tx), ' steps');
 
 % Plot the Data
 figure(1); clf; hold on; 
-plot(t_23, y_23, 'b.', 'DisplayName', label_23);
-plot(t_tx, y_tx, 'r.', 'DisplayName', label_tx);
+fplot(y_ds, [0, 1], 'g','DisplayName', 'dsolve', 'LineWidth',2);
+plot(t_23, y_23, 'b.', 'DisplayName', label_23, 'LineWidth',2);
+plot(t_tx, y_tx, 'r.', 'DisplayName', label_tx, 'LineWidth',2);
 hold off; 
 ttl = title('Amelia Rotondo, 7.6: Comparing ODE Solvers');
 lgd = legend('Location', 'best');
 fontsize(ttl,18,'points');
 fontsize(lgd,14,'points');
 lgd; ttl;
+fprintf('Number of Steps for ode23  : %d\n', length(t_23))
+fprintf('Number of Steps for ode23tx: %d\n', length(t_tx))
+
 
 %
